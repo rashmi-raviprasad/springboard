478588b8b0b069947f6222a7523f95314a524dad
rashmi <rashmi.raviprasad@gmail.com>
2019-08-19 19:12:41 -0700

    sorted column names into categories

--- capstone1/data_wrangling.py
+++ capstone1/data_wrangling.py
@@ -4,7 +4,7 @@ import pandas as pd
 url = 'http://vfacstaff.ltu.edu/lshamir/data/assym/p_all_full.csv'
 file_loc = 'data.csv'
 df = pd.read_csv(file_loc, nrows=10, index_col=0)
-petro = df.columns.str.contains('petro')
 petro_df = df.filter(like='petro', axis=1)
 print(petro_df.head())
-#data.to_csv('data.csv')
+col_names = pd.read_csv('col_names.csv')
+print(col_names)
