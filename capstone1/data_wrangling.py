9c513a62b590e1ab461259383337c29627a27c77
rashmi <rashmi.raviprasad@gmail.com>
2019-08-20 17:55:47 -0700

    formatted dataset

--- capstone1/data_wrangling.py
+++ capstone1/data_wrangling.py
@@ -1,10 +1,48 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+def subset_df(df, group):
+    '''
+    Method for creating a new dataframe for a specified group. 
+    Preserves the 'rotation' column from the original dataframe.
+    '''
+    new_df = df[['rotation', group]].droplevel(0, axis=1)
+    new_df.rename(columns={new_df.columns[0]:'rotation'}, inplace=True)
+    return new_df
+
 
 url = 'http://vfacstaff.ltu.edu/lshamir/data/assym/p_all_full.csv'
 file_loc = 'data.csv'
-df = pd.read_csv(file_loc, nrows=10, index_col=0)
-petro_df = df.filter(like='petro', axis=1)
-print(petro_df.head())
-col_names = pd.read_csv('col_names.csv')
-print(col_names)
+df = pd.read_csv(file_loc, index_col=0)
+
+col_names = pd.read_csv('col_names.csv', names=['category', 'col_name'])
+col_groups = col_names.groupby('category')['col_name'].apply(list).to_dict()
+
+data_df = pd.concat([df[col_groups[key]] for key in col_groups.keys()], 
+            axis=1, keys=col_groups.keys())
+
+data_df.insert(0, 'rotation', df['rotation'])
+
+coord_df = subset_df(data_df, 'coordinates')
+sns.scatterplot(x='ra', y='dec', data=coord_df, hue='rotation', alpha=0.5, palette='dark', s=3)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
