eaf96b4c20c237eaec763da3be2d57eb912d1d97
rashmi <rashmi.raviprasad@gmail.com>
2019-10-18 21:28:48 -0700

    work on statistical inference

--- capstone1/statistical_inference.py
+++ capstone1/statistical_inference.py
@@ -75,17 +75,52 @@ df_list.append(types)
 # crude way to perform feature selection.
 
 significant_cols = []
-bands = ['u', 'g', 'r', 'i', 'z']
+#bands = ['u', 'g', 'r', 'i', 'z']
 for df in df_list:
-    for col in df.columns:
-        for band in bands:
-            try:
-                t_val, p_val = stats.ttest_ind(get_band(get_cw(df).dropna(), band)[col], 
-                            get_band(get_ccw(df).dropna(), band)[col])
-                if p_val <= 0.05:
-                    print(col, t_val, p_val)
-                    significant_cols.append(col)
-            except:
-                continue
+    for col in df.columns[1:]:
+        cw_variable = get_cw(df)[col].dropna()
+        ccw_variable = get_ccw(df)[col].dropna()
+        cw_mean_estimate = np.mean(cw_variable)
+        cw_std_estimate = np.std(cw_variable, ddof=1)
+        ccw_mean_estimate = np.mean(ccw_variable)
+        ccw_std_estimate = np.std(ccw_variable, ddof=1)
+        cw_crit_val = stats.t.ppf(0.05, loc=cw_mean_estimate, scale=cw_std_estimate, df=13439)
+        ccw_crit_val = stats.t.ppf(0.05, loc=ccw_mean_estimate, scale=ccw_std_estimate, df=13439)
+        cw_margin = abs(cw_mean_estimate - cw_crit_val)
+        ccw_margin = abs(ccw_mean_estimate - ccw_crit_val)
 
-print(significant_cols)
+        t_val, p_val = stats.ttest_ind(cw_variable, ccw_variable, equal_var=False)
+        if p_val <= 0.05:
+            print('\nname:', col, 't:', t_val, 'p:', p_val)
+            print('cw mean:', cw_mean_estimate, 'cw low:', cw_mean_estimate - cw_margin, 
+                        'cw high:', cw_mean_estimate + cw_margin)
+            print('ccw mean:', ccw_mean_estimate, 'ccw low:', ccw_mean_estimate - ccw_margin, 
+                        'ccw high:', ccw_mean_estimate + ccw_margin)
+            significant_cols.append(col)
+        
+
+#print(significant_cols)
+'''
+plt.plot(get_cw(exponential)['expPhi_u'], get_cw(devaucouleurs)['deVPhi_u'], color='green', linestyle='none', marker='.', alpha=0.5, markersize=0.5)
+plt.plot(get_ccw(exponential)['expPhi_u'], get_ccw(devaucouleurs)['deVPhi_u'], color='yellow', linestyle='none', marker='.', alpha=0.25, markersize=0.5)
+
+plt.show()
+'''
+
+#mean_diff = np.mean(get_cw(stokes)['u_u']) - np.mean(get_ccw(stokes)['u_u'])
+
+plt.subplot(211)
+plt.hist(get_cw(petro)['petroMag_u'], alpha=0.5)
+plt.axvline(np.mean(get_cw(petro)['petroMag_u']))
+
+plt.subplot(212)
+plt.hist(get_ccw(petro)['petroMag_u'], alpha=0.5)
+plt.axvline(np.mean(get_ccw(petro)['petroMag_u']))
+
+plt.show()
+
+r_1, p_1 = stats.pearsonr(get_cw(exponential)['expRad_z'], get_cw(exponential)['expMag_z'])
+r_2, p_2 = stats.pearsonr(get_ccw(exponential)['expRad_z'], get_ccw(exponential)['expMag_z'])
+
+print(r_1, p_1)
+print(r_2, p_2)
